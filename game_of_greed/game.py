fb8dd267930b24d6c3a2faa71f99227bb2e16472
royce79-creator <connorsboyce07@gmail.com>
2022-01-08 15:33:01 -0800

    Made changes for lab 08

--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -2,82 +2,122 @@ from game_of_greed.game_logic import GameLogic
 from game_of_greed.banker import Banker
 import sys
 
-welcome_message = """
-**************************************************
-***                                            ***
-***   Welcome to the Game of greed, have fun!   ***
-***                                            ***
-**************************************************
-May the odds ever be in your favor! To start type: Start. If not type "quit" to exit. 
-"""
-# Want to prompt the user to start making a choise with either starting or quitting using available options "start" or "quit"
+class Game:
+    def __init__(self):
+        self.round_counter = 0
+        self.dice_qty = 6
+        self.banker = Banker()
 
-class Game: 
-  def __init__(self):
-    self.round_counter = 0
-    self.dice_amount = 6
-    self.banker = Banker()
-  
-  def main_roller(self):
-    GameLogic.roll_dice(self.dice_amount)
+    def welcome(self):
+        print("Welcome to Game of Greed")
 
-  def play(self, roller = GameLogic.roll_dice):
-    print("Welcome to Game of Greed")
-    print("(y)es to play or (n)o to decline")
-    response = input('> ')
+    def play(self, roller = GameLogic.roll_dice):
+        self.welcome()
+        print("(y)es to play or (n)o to decline")
+        response = input("> ")
+        if response == "y":
+            self.run_game(roller)
+        elif response == "n":
+            print("OK. Maybe another time")
+            self.quit()
+        else:
+            return self.play(roller)
 
-    if response == 'n':
-      print('OK. Maybe another time')
-    elif response == 'y':
-      self.run_game(roller)
-  
-  def run_game(self, roller):
+    def run_game(self, roller):
+        self.start_round(roller)
 
-    while True:
-      self.round_counter += 1
+    def roll_loop(self, roller):
+        rolled_dice = self.roll(roller)
+        selected_dice = self.select_dice_and_validate(rolled_dice, roller)
+        self.calculate_score(selected_dice)
+        self.show_score_and_dice_qty()
+        self.prompt_and_handle_bank_reroll_or_quit(roller)
 
-      print(f'Starting round {self.round_counter}')
-      print(f'Rolling {self.dice_amount} dice...')
+    def start_round(self, roller):
+        self.round_counter += 1
+        self.dice_qty = 6
+        self.banker.clear_shelf()
+        print(f"Starting round {self.round_counter}")
+        self.roll_loop(roller)
 
-      rolled_dice = roller(self.dice_amount)
-      formatted_dice = str(rolled_dice).strip('([])').replace(",","")
-      print('*** ' + formatted_dice + ' ***')
+    def roll(self, roller):
+        print(f"Rolling {self.dice_qty} dice...")
+        rolled_dice = roller(self.dice_qty)
+        self.show_rolled_dice(rolled_dice)
+        return rolled_dice
 
-      if GameLogic.calculate_score(rolled_dice) == 0:
-        print('Farkled')
-        continue
+    def show_rolled_dice(self, rolled_dice):
+        formatted_dice = str(rolled_dice).strip("([])").replace(", ", " ")  # (4, 4, 5, 2, 3, 1) -> *** 4 4 5 2 3 1 ***
+        print("*** " + formatted_dice + " ***")
 
-      print('Enter dice to keep, or (q)uit:')
-      response = input('> ')
+    def select_dice_and_validate(self, rolled_dice, roller):
 
-      if response == 'q':
-        print(f'Thanks for playing. You earned {self.banker.balance} points')
-        sys.exit()
+        if GameLogic.calculate_score(rolled_dice) == 0:
+            print("****************************************")
+            print("**        Zilch!!! Round over         **")
+            print("****************************************")
+            self.banker.clear_shelf()
+            self.bank_points(roller)
 
-      else:
-        response_list = tuple(map(int, list(response)))
-        score = GameLogic.calculate_score(response_list)
+        else:
+            print("Enter dice to keep, or (q)uit:")
+            response = input("> ")
+            if response:
+                response = response.replace(" ", "")
+            else:
+                self.select_dice_and_validate(rolled_dice, roller)
+
+            if response == "q":
+                print(f"Thanks for playing. You earned {self.banker.balance} points")
+                self.quit()
+
+            response_to_tuple = tuple([int(char) for char in response])
+            isValid = GameLogic.validate_input(rolled_dice, response_to_tuple)
+            if isValid:
+                return response_to_tuple
+            else:
+                print("Cheater!!! Or possibly made a typo...")
+                self.show_rolled_dice(rolled_dice)
+                return self.select_dice_and_validate(rolled_dice, roller)
+
+    def calculate_score(self, selected_dice):
+        score = GameLogic.calculate_score(selected_dice) ## response = "1234" --> [1, 2, 3, 4] --> (1, 2, 3, 4)
         self.banker.shelf(score)
-        self.dice_amount -= len(response_list)
-        print(f'You have {self.banker.shelved} unbanked points and {self.dice_amount} dice remaining')
-        print('(r)oll again, (b)ank your points or (q)uit:')
-        response = input('> ')
+        self.dice_qty -= len(selected_dice)
+        return score
 
-      if response == 'b':
+    def show_score_and_dice_qty(self):
+        print(f"You have {self.banker.shelved} unbanked points and {self.dice_qty} dice remaining")
+
+    def prompt_and_handle_bank_reroll_or_quit(self, roller):
+        print("(r)oll again, (b)ank your points or (q)uit:")
+        bank_reroll_or_quit = input("> ").replace(" ", "")
+
+        if bank_reroll_or_quit == "q":
+            print(f"Thanks for playing. You earned {self.banker.balance} points")
+            self.quit()
+        elif bank_reroll_or_quit == "r":
+            if self.dice_qty == 0:
+                self.dice_qty = 6
+                self.roll_loop(roller)
+            else:
+                self.roll_loop(roller)
+        elif bank_reroll_or_quit == "b":
+            self.bank_points(roller)
+
+    def bank_points(self, roller):
+        print(f"You banked {self.banker.shelved} points in round {self.round_counter}")
         self.banker.bank()
-        self.dice_amount = 6
-        print(f'You banked {score} points in round {self.round_counter}')
-        print(f'Total score is {self.banker.balance} points')
-      
-      elif response == 'r':
-        if self.dice_amount == 0:
-          self.dice_amount = 6
-        continue
+        print(f"Total score is {self.banker.balance} points")
+        
+        if self.banker.balance >= 10000:
+            print("WINNER")
+        else:
+            self.start_round(roller)
 
-      elif response == 'q':
-        print(f'Thanks for playing. You earned {self.banker.balance} points')
+    def quit(self):
         sys.exit()
 
 if __name__ == "__main__":
-  game = Game()
-  game.play()
+    game = Game()
+    game.play()
---
+++ game_of_greed/og_game.py
@@ -0,0 +1,83 @@
+from game_of_greed.game_logic import GameLogic
+from game_of_greed.banker import Banker
+import sys
+
+welcome_message = """
+**************************************************
+***                                            ***
+***   Welcome to the Game of greed, have fun!   ***
+***                                            ***
+**************************************************
+May the odds ever be in your favor! To start type: Start. If not type "quit" to exit. 
+"""
+# Want to prompt the user to start making a choise with either starting or quitting using available options "start" or "quit"
+
+class Game: 
+  def __init__(self):
+    self.round_counter = 1
+    self.dice_amount = 6
+    self.banker = Banker()
+  
+  def main_roller(self):
+    GameLogic.roll_dice(self.dice_amount)
+
+  def play(self, roller = GameLogic.roll_dice):
+    print("Welcome to Game of Greed")
+    print("(y)es to play or (n)o to decline")
+    response = input('> ')
+
+    if response == 'n':
+      print('OK. Maybe another time')
+    elif response == 'y':
+      self.run_game(roller)
+  
+  def run_game(self, roller):
+
+    while True:
+      self.round_counter += 1
+
+      print(f'Starting round {self.round_counter}')
+      print(f'Rolling {self.dice_amount} dice...')
+
+      rolled_dice = roller(self.dice_amount)
+      formatted_dice = str(rolled_dice).strip('([])').replace(",","")
+      print('*** ' + formatted_dice + ' ***')
+
+      if GameLogic.calculate_score(rolled_dice) == 0:
+        print('Farkled')
+        continue
+
+      print('Enter dice to keep, or (q)uit:')
+      response = input('> ')
+
+      if response == 'q':
+        print(f'Thanks for playing. You earned {self.banker.balance} points')
+        sys.exit()
+
+      else:
+        response_list = tuple(map(int, list(response)))
+        score = GameLogic.calculate_score(response_list)
+        self.banker.shelf(score)
+        self.dice_amount -= len(response_list)
+        print(f'You have {self.banker.shelved} unbanked points and {self.dice_amount} dice remaining')
+        print('(r)oll again, (b)ank your points or (q)uit:')
+        response = input('> ')
+
+      if response == 'b':
+        self.banker.bank()
+        self.dice_amount = 6
+        print(f'You banked {score} points in round {self.round_counter}')
+        print(f'Total score is {self.banker.balance} points')
+      
+      elif response == 'r':
+        if self.dice_amount == 0:
+          self.dice_amount = 6
+        continue
+
+      elif response == 'q':
+        print(f'Thanks for playing. You earned {self.banker.balance} points')
+        sys.exit()
+
+if __name__ == "__main__":
+  game = Game()
+  game.play()
